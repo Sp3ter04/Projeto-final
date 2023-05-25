cfa1bd748060cf799b743a4108a9351890df30dc
Jose-Melicias <jmlm3103@gmail.com>
2023-05-25 21:22:42 +0100

    Added obstacles.py

--- waiter.py
+++ waiter.py
@@ -1,20 +1,41 @@
 import graphics
 
 class Waiter:
-    def __init__(self):
+    def __init__(self, color, size):
         pass
+
+    def draw(self, win):
+        pass
+
+    def move(self, dx, dy):
+        pass
+
+    def spin(self):
+        pass
+
+    def back_to_docking(self):
+        pass
+
             
 
 class Waiter1(Waiter):
     def __init__(self):
         super().__init__(self)
 
+    def clean_room(self):
+        pass
+
 class Waiter2(Waiter):
     def __init__(self):
         super().__init__(self)
+
+    def sweep_whole_room(self):
+        pass
 
 class Waiter3(Waiter):
     def __init__(self):
         super().__init__(self)
 
+    def clean_room(self):
+        pass
 
