d241f64607b02b40866724d47d08084d62f4b375
Jose-Melicias <jmlm3103@gmail.com>
2023-05-25 21:08:12 +0100

    Waiter.py
    Waiter file is added

---
+++ waiter.py
@@ -0,0 +1,20 @@
+import graphics
+
+class Waiter:
+    def __init__(self):
+        pass
+            
+
+class Waiter1(Waiter):
+    def __init__(self):
+        super().__init__(self)
+
+class Waiter2(Waiter):
+    def __init__(self):
+        super().__init__(self)
+
+class Waiter3(Waiter):
+    def __init__(self):
+        super().__init__(self)
+
+
